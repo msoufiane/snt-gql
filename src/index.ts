e16613c51101176b6aa482f3393a5e63f6e94a26
Soufiane Mghanen <mgh.soufiane@gmail.com>
2019-01-21 16:10:31 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,54 @@ import { ApolloServer } from 'apollo-server-express';
 import Express from 'express';
 import { buildSchema, formatArgumentValidationError } from 'type-graphql';
 import { createConnection } from 'typeorm';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import cors from 'cors';
 
 import { RegisterResolver } from './modules/user/Register';
+import { LoginResolver } from './modules/user/Login';
+import { redis } from './redis';
 
 const main = async () => {
 	await createConnection();
 
 	const schema = await buildSchema({
-		resolvers: [RegisterResolver],
+		resolvers: [RegisterResolver, LoginResolver],
 	});
 
 	const apolloServer = new ApolloServer({
 		schema,
 		formatError: formatArgumentValidationError,
+		context: ({ req }: any) => ({ req }),
 	});
 
 	const app = Express();
+
+	const RedisStore = connectRedis(session);
+
+	app.use(
+		cors({
+			credentials: true,
+			origin: 'http://localhost:4000',
+		})
+	);
+
+	app.use(
+		session({
+			store: new RedisStore({
+				client: redis as any,
+			}),
+			name: 'qid',
+			secret: 'SESSION_SECRET',
+			resave: false,
+			saveUninitialized: false,
+			cookie: {
+				httpOnly: true,
+				secure: process.env.NODE_ENV === 'production',
+				maxAge: 1000 * 60 * 60 * 24 * 7 * 365,
+			},
+		})
+	);
 
 	apolloServer.applyMiddleware({ app });
 
